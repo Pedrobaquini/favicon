bed9fd6e914eb5537867ae395352c9a666b7c1c5
Pedro Arão Baquini <106710888+Pedrobaquini@users.noreply.github.com>
2024-10-11 09:37:42 -0300

    testes

---
+++ script.js
@@ -0,0 +1,11 @@
+window.alert('Hello')
+
+function printar() {
+
+    window.alert('Hello Again')
+}
+function logar(){
+var botao = document.getElementById('btnLogar')
+// window.location.href='https://www.google.com.br/'
+
+}
